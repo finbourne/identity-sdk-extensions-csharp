8cf2393fb39ac97d07477781e273f2c2645ea9d6
Concourse <concourse@finbourne.com>
2022-02-24 11:19:37 +0000

    Update SDK Extensions 0.2.0 - autogenerated by MustacheToCSharpGenerator

--- sdk/Finbourne.Identity.Sdk.Extensions/ApiResponseExtensions.cs
+++ sdk/Finbourne.Identity.Sdk.Extensions/ApiResponseExtensions.cs
@@ -11,7 +11,7 @@ namespace Finbourne.Identity.Sdk.Extensions
         /// <summary>
         /// The header that the request id is contained within.
         /// </summary>
-        public const string RequestIdHeader = "identity-meta-requestId";
+        public const string RequestIdHeader = "lydia-meta-requestId";
         
         /// <summary>
         /// The header that the Date Time Offset is contained within.
