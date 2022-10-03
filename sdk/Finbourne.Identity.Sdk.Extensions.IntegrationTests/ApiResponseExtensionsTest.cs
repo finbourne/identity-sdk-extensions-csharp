1597ddde38e66d35afad54f31d50fafaeaeff5e7
robert.aldis <robert.aldis@finbourne.com>
2022-10-03 10:53:11 +0100

    IAM-1263: Update tests to point to non hidden endpoints

--- sdk/Finbourne.Identity.Sdk.Extensions.IntegrationTests/ApiResponseExtensionsTest.cs
+++ sdk/Finbourne.Identity.Sdk.Extensions.IntegrationTests/ApiResponseExtensionsTest.cs
@@ -18,7 +18,7 @@ namespace Finbourne.Identity.Sdk.Extensions.IntegrationTests
         [Test]
         public void GetRequestId_CanExtract_RequestId()
         {
-            var apiResponse = _factory.Api<ApplicationMetadataApi>().GetRegistrationAvailabilityWithHttpInfo();
+            var apiResponse = _factory.Api<ApplicationMetadataApi>().ListAccessControlledResourcesWithHttpInfo();
             var requestId = apiResponse.GetRequestId();
             StringAssert.IsMatch(RequestIdRegexPattern, requestId);
         }
@@ -26,7 +26,7 @@ namespace Finbourne.Identity.Sdk.Extensions.IntegrationTests
         [Test]
         public void GetRequestId_MissingHeader_ReturnsNull_RequestId()
         {
-            var apiResponse = _factory.Api<ApplicationMetadataApi>().GetRegistrationAvailabilityWithHttpInfo();
+            var apiResponse = _factory.Api<ApplicationMetadataApi>().ListAccessControlledResourcesWithHttpInfo();
             // Remove header containing access token
             apiResponse.Headers.Remove(ApiResponseExtensions.RequestIdHeader);
             var requestId = apiResponse.GetRequestId();
@@ -36,7 +36,7 @@ namespace Finbourne.Identity.Sdk.Extensions.IntegrationTests
         [Test]
         public void GetRequestDateTime_CanExtract_DateHeader()
         {
-            var apiResponse = _factory.Api<ApplicationMetadataApi>().GetRegistrationAvailabilityWithHttpInfo();
+            var apiResponse = _factory.Api<ApplicationMetadataApi>().ListAccessControlledResourcesWithHttpInfo();
             var date = apiResponse.GetRequestDateTime();
             Assert.IsNotNull(date);
         }
@@ -44,7 +44,7 @@ namespace Finbourne.Identity.Sdk.Extensions.IntegrationTests
         [Test]
         public void GetRequestDateTime_InvalidDateHeader_ReturnsNull_DateHeader()
         {
-            var apiResponse = _factory.Api<ApplicationMetadataApi>().GetRegistrationAvailabilityWithHttpInfo();
+            var apiResponse = _factory.Api<ApplicationMetadataApi>().ListAccessControlledResourcesWithHttpInfo();
             // Invalidate header containing access token
             apiResponse.Headers[ApiResponseExtensions.DateHeader] = new[] { "invalid" };
             var date = apiResponse.GetRequestDateTime();
@@ -54,7 +54,7 @@ namespace Finbourne.Identity.Sdk.Extensions.IntegrationTests
         [Test]
         public void GetRequestDateTime_MissingHeader_ReturnsNull_DateHeader()
         {
-            var apiResponse = _factory.Api<ApplicationMetadataApi>().GetRegistrationAvailabilityWithHttpInfo();
+            var apiResponse = _factory.Api<ApplicationMetadataApi>().ListAccessControlledResourcesWithHttpInfo();
             // Remove header containing access token
             apiResponse.Headers.Remove(ApiResponseExtensions.DateHeader);
             var date = apiResponse.GetRequestDateTime();
